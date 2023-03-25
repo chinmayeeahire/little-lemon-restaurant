cd37c1f45250b90f63bf3a86af021c2c8f26db69
Chinmayee Ahire <theroadtotae@gmail.com>
2023-03-26 01:39:07 +0530

    Semantic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      Homepage
+      <Header />
+      <Navbar/>
+      <Main />
+      <Footer />
+      
     </div>
   );
 }
